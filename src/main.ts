51f9e3578386038e7d97a9a92622b531802c6b75
MyUsername <rosanio.c@gmail.com>
2025-04-18 10:28:41 +0200

    Snack 1 - BONUS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ if (typeof dato === 'string') {
   console.log(dato * 2);
 } else if (typeof dato === 'boolean') {
   console.log(dato ? 'Si' : 'No');
+} else if (dato === null) {
+  console.log('Il dato è vuoto')
+  // ARRAY v1 - IS ARRAY
+} else if (Array.isArray(dato)) {
+  console.log(dato.length);
+  // ARRAY v2 - INSTANCE OF
+} else if (dato instanceof Array) {
+  console.log(dato.length);
+} else if (dato instanceof Promise) {
+  dato.then((msg) => console.log(msg))
 } else {
   console.log('Tipo non supportato');
 }
